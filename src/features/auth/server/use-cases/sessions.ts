ab21b3ec8af39b6054293ea4086c401652d739df
Shamiun Shadid <shamiunshadid@gmail.com>
2026-02-16 21:17:26 +0600

    Added function for managing user sessions.

--- src/features/auth/server/use-cases/sessions.ts
+++ src/features/auth/server/use-cases/sessions.ts
@@ -3,7 +3,7 @@ import crypto from "crypto";
 import { getIpAdress } from "./location";
 import { db } from "@/config/db";
 import { sessions, users } from "@/drizzle/schema";
-import { SESSION_LIFETIME } from "@/config/constants";
+import { SESSION_LIFETIME, SESSION_REFRESH_TIME } from "@/config/constants";
 import { eq } from "drizzle-orm";
 
 
@@ -81,5 +81,26 @@ export const validateSessionAndGetUser = async(session: string)=>{
   .where(eq(sessions.id, hashedToken))
   .innerJoin(users, eq(users.id, sessions.userId))
 
+
+  if(!user) return null;
+
+  if(Date.now() >= user.session.expiresAt.getTime()){
+    await invalidateSession(user.session.id)
+    return null
+  }
+
+  if(Date.now() >= user.session.expiresAt.getTime() - SESSION_REFRESH_TIME * 1000){
+    await db.update(sessions)
+    .set({
+      expiresAt: new Date(Date.now() + SESSION_LIFETIME * 1000),
+    })
+    .where(eq(sessions.id, user.session.id))
+  }
+
+
   return user;
+}
+
+const invalidateSession = async(id: string)=>{
+  await db.delete(sessions).where(eq(sessions.id, id))
 }
