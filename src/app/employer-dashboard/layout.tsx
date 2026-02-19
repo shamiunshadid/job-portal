234eadaa03fdd94c763dc9a7bc3c834b45bc8aee
Shamiun Shadid <shamiunshadid@gmail.com>
2026-02-19 23:20:42 +0600

    Created dashboard and employer dashboard.

---
+++ src/app/employer-dashboard/layout.tsx
@@ -0,0 +1,15 @@
+import { getCurrentUser } from "@/features/auth/server/auth.queries";
+import { redirect } from "next/navigation";
+import React from "react";
+
+export default async function DashboardLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  const user = await getCurrentUser();
+
+  if (!user) return redirect("/login");
+
+  if(user.role !== "employer") return redirect("/dashboard")
+
+  return <>{children}</>;
+}
---
+++ src/app/employer-dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EmployerDashboard = () => {
+  return (
+    <div>
+        <h1>Welcome employer</h1>
+    </div>
+  )
+}
+
+export default EmployerDashboard
